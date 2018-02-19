c9c922812f73e14425b21704830cc7fbcc64702e
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-02-19 18:54:02 +0100

    Add game events

--- ssl-status-board-server.go
+++ ssl-status-board-server.go
@@ -25,14 +25,23 @@ type Team struct {
 }
 
 type Stage struct {
-	Id       int
 	Name     string
 	TimeLeft int
 }
 
 type Command struct {
-	Id   int
 	Name string
+}
+
+type Originator struct {
+	Team  string
+	BotId int
+}
+
+type GameEvent struct {
+	Type       string
+	Originator Originator
+	Message    string
 }
 
 type Referee struct {
@@ -40,11 +49,13 @@ type Referee struct {
 	Command    Command
 	TeamYellow Team
 	TeamBlue   Team
+	GameEvent  GameEvent
 }
 
-var referee = Referee{Stage{0, "NORMAL_FIRST_HALF_PRE", 1000}, Command{0, "HALT"},
+var referee = Referee{Stage{"NORMAL_FIRST_HALF_PRE", 1000}, Command{"HALT"},
 	Team{"yellow", 5, 0, 1, []uint32{5000}, 1, 10000},
-	Team{"blue", 1, 1, 3, []uint32{}, 2, 20000}}
+	Team{"blue", 1, 1, 3, []uint32{}, 2, 20000},
+	GameEvent{"UNKNOWN", Originator{"UNKNOWN", -1}, "Custom message"}}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -127,17 +138,29 @@ func handleIncomingRefereeMessages() {
 }
 
 func saveRefereeMessageFields(message *sslproto.SSL_Referee) {
-	referee.Stage.Id = int(*message.Stage)
 	referee.Stage.Name = message.Stage.String()
 	if message.StageTimeLeft != nil {
 		referee.Stage.TimeLeft = int(*message.StageTimeLeft)
 	} else {
 		referee.Stage.TimeLeft = 0
 	}
-	referee.Command.Id = int(*message.Command)
 	referee.Command.Name = message.Command.String()
 	referee.TeamYellow = mapTeam(message.Yellow)
 	referee.TeamBlue = mapTeam(message.Blue)
+	referee.GameEvent.Originator.Team = message.GameEvent.Originator.Team.String()
+	if message.GameEvent != nil {
+		if message.GameEvent.Originator.BotId == nil {
+			referee.GameEvent.Originator.BotId = -1
+		} else {
+			referee.GameEvent.Originator.BotId = int(*message.GameEvent.Originator.BotId)
+		}
+		if message.GameEvent.Message == nil {
+			referee.GameEvent.Message = ""
+		} else {
+			referee.GameEvent.Message = *message.GameEvent.Message
+		}
+		referee.GameEvent.Type = message.GameEvent.GameEventType.String()
+	}
 }
 
 func mapTeam(teamInfo *sslproto.SSL_Referee_TeamInfo) (team Team) {
